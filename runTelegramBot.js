07bd590ab949dbd3cf17f1e0555a13140a5ce36a
bongjinpark1 <bongjinpark1@gmail.com>
2019-07-14 21:48:39 +0900

    logger match only

--- runTelegramBot.js
+++ runTelegramBot.js
@@ -5,6 +5,7 @@ const finders = require('./handlers/finders')
 module.exports = (bot) => {
   bot.use((ctx, next) => {
     const start = new Date()
+    if (!ctx.match) next()
     return next(ctx).then(() => {
       if (!ctx.update.callback_query) {
         const ms = new Date() - start
