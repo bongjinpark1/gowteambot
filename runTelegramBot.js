b8b3324b38cf0ebbea83b46795845967e094fe59
bongjinpark1 <bongjinpark1@gmail.com>
2019-07-14 21:05:35 +0900

    로거 추가

--- runTelegramBot.js
+++ runTelegramBot.js
@@ -6,14 +6,31 @@ module.exports = (bot) => {
   bot.use((ctx, next) => {
     const start = new Date()
     return next(ctx).then(() => {
-      const ms = new Date() - start
-      console.log('Response time %sms', ms)
+      if (!ctx.update.callback_query) {
+        const ms = new Date() - start
+        const from = ctx.update.message.from.first_name
+        const input = ctx.update.message.text
+        let message = ''
+        message += `from: ${from}\n`
+        message += `input: ${input}\n`
+        message += `latancy: ${ms} ms`
+        ctx.telegram.sendMessage(process.env.ADMIN_CHAT_ID, message)
+      }
     })
   })
   teamcodeParser.attach(bot)
   setImageYOffset.attach(bot)
   editComment.attach(bot)
   finders.attach(bot)
+  bot.command('help', (context) => {
+    let message = ''
+    message += '[teamcode] 팀코드를 해독하여 출력합니다.\n\n'
+    message += '/find #hashtag, @username, troopname 으로 팀을 검색합니다. 코멘트가 없는 팀은 검색되지 않습니다.\n\n'
+    message += '/ec [nnnnn] [comment] 내가 포스팅한 팀의 코멘트를 덮어씁니다.\n\n'
+    message += '/iy [cardId] [number] 카드 이미지의 y 오프셋을 변경합니다. 기본값 100에서 작아지면 올라가고, 커지면 내려갑니다.\n\n'
+
+    context.reply(message)
+  })
   bot.launch()
   console.log('Bot running...')
 }
