aba401d970076b7fd6f3cb79a752ff98bbd7b73c
bongjinpark1 <bongjinpark1@gmail.com>
2019-10-18 16:41:59 +0900

    rollback

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ dbConnect(() => {
   run(bot)
 })
 
-const { fork } = require('child_process')
-const path = require('path')
+// const { fork } = require('child_process')
+// const path = require('path')
 
-let crawler
+// let crawler
 
-function createCrawler () {
-  crawler = fork(path.join(__dirname, './clienCrawler.js'), {}, 5000)
+// function createCrawler () {
+//   crawler = fork(path.join(__dirname, './clienCrawler.js'), {}, 5000)
 
-  crawler.on('exit', () => {
-    console.log('crawler exited.')
-    createCrawler()
-  })
-}
+//   crawler.on('exit', () => {
+//     console.log('crawler exited.')
+//     createCrawler()
+//   })
+// }
 
-createCrawler()
+// createCrawler()
