a07525978470c9d31165223c5f203d14bdc88769
bongjinpark1 <bongjinpark1@gmail.com>
2019-10-18 16:17:55 +0900

    add clien crawler

---
+++ clienCrawler.js
@@ -0,0 +1,40 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+const Telegram = require('telegraf/telegram')
+
+const telegram = new Telegram(process.env.BOT_TOKEN)
+const world = -1001077486754
+
+const http = axios.create({
+  baseURL: 'https://www.clien.net/service/board/cm_gemwar'
+})
+
+let last = 14178205
+
+function job () {
+  http.get('/')
+    .then((res) => {
+      const $ = cheerio.load(res.data)
+
+      const arr = $('body .nav_container .content_list .list_content .list_item').map((index, el) => {
+        const key = $(el).attr('data-board-sn')
+        const href = $(el).find('.list_title .list_subject').attr('href')
+
+        return {
+          key, href
+        }
+      }).get()
+
+      arr.reverse().forEach(({ key, href }) => {
+        if (!last || Number(last) < Number(key)) {
+          telegram.sendMessage(world, href)
+          last = key
+        }
+      })
+    })
+    .catch((err) => {
+      console.error(err.message)
+    })
+}
+
+setInterval(job, 1000 * 60)
--- index.js
+++ index.js
@@ -6,3 +6,19 @@ const run = require('./runTelegramBot')
 dbConnect(() => {
   run(bot)
 })
+
+const { fork } = require('child_process')
+const path = require('path')
+
+let crawler
+
+function createCrawler () {
+  crawler = fork(path.join(__dirname, './clienCrawler.js'), {}, 5000)
+
+  crawler.on('exit', () => {
+    console.log('crawler exited.')
+    createCrawler()
+  })
+}
+
+createCrawler()
